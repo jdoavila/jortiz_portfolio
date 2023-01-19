e8f56e5890e405ca01851ef3841af12bd67356cb
Daniel Ortiz <jdoavila@gmail.com>
2023-01-18 22:39:28 -0600

    Mejoras en el index

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,54 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="utf-8" />
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+        <meta
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          name="viewport"
+        />
+        <meta name="theme-color" content="#5540af" />
+        <meta property="og:site_name" content="Daniel Ortiz Porfolio" />
+        <meta name="twitter:site" content="@danielortiz_d" />
+
+        <link
+          crossorigin="crossorigin"
+          href="https://fonts.gstatic.com"
+          rel="preconnect"
+        />
+        <link
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
+          rel="preload"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+
+        <script
+          defer
+          src="https://unpkg.com/@alpine-collective/toolkit@1.0.0/dist/cdn.min.js"
+        ></script>
+
+        <script
+          defer
+          src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
